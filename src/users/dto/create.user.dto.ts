98833bc4b83032d8d0dd37872ebc5f20c076e564
viktor <viktorvanyatanya@gmail.com>
2022-09-08 18:54:02 +0200

    start nest js

---
+++ src/users/dto/create.user.dto.ts
@@ -0,0 +1,15 @@
+import {ApiProperty} from '@nestjs/swagger';
+
+export class CreateUserDto {
+
+    @ApiProperty({example: 'user@mail.com', description: 'email'})
+    readonly email: string;
+
+    @ApiProperty({example: 'password11', description: 'password'})
+    readonly password: string;
+
+    @ApiProperty({example: 'Sumy', description: 'city'})
+    readonly city: string;
+
+    readonly status?: boolean;
+}
---
+++ src/users/users.model.ts
@@ -0,0 +1,27 @@
+import {Column, DataType, Model, Table} from 'sequelize-typescript';
+
+interface IUser {
+    id: number;
+    email: string;
+    password: string;
+    city: string;
+    status: boolean;
+}
+@Table({tableName: 'users'})
+export class User extends Model<User, IUser> {
+
+    @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @Column({ type: DataType.STRING, unique: true, allowNull: false })
+    email: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    password: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    city: string;
+
+    @Column({ type: DataType.BOOLEAN })
+    status: boolean;
+}
